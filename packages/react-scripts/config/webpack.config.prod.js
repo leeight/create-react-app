d368a0c7f14b919ebdafe9cae54eb3ee0ee66b10
majunchen <majunchen@bytedance.com>
2018-09-06 18:14:10 +0800

    增加 build --size 参数 查看包大小 git push
    Change-Id: I364f0a17ef3793824af5fbf5302767d5e3180a88

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -565,17 +565,23 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new ModuleConcatenationPlugin()
-  ].concat(
-    LANG === 'typescript'
-      ? [
-          new ForkTsCheckerWebpackPlugin({
-            async: false,
-            tsconfig: paths.appTsProdConfig,
-            tslint: paths.appTsLint
-          })
-        ]
-      : []
-  ),
+  ]
+    .concat(
+      LANG === 'typescript'
+        ? [
+            new ForkTsCheckerWebpackPlugin({
+              async: false,
+              tsconfig: paths.appTsProdConfig,
+              tslint: paths.appTsLint
+            })
+          ]
+        : []
+    )
+    .concat(
+      process.argv.slice(1).indexOf('--size') !== -1
+        ? [new require('webpack-bundle-analyzer').BundleAnalyzerPlugin()]
+        : []
+    ),
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
